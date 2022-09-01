f282ec8ee6aa6c164e89c5d27611101f880ea969
Shaul Attie <solyattie13@gmail.com>
2022-09-01 09:41:39 +0300

    axios api update

--- src/Redux/api/index.js
+++ src/Redux/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const API = axios.create({baseURL: 'http://localhost:3001'})
+// const API = axios.create({baseURL: 'http://localhost:3001'})
+const API = axios.create({baseURL: 'https://musicplyr13.herokuapp.com/'})
 
 API.interceptors.request.use((req)=> {
     if(localStorage.getItem('profile')){
